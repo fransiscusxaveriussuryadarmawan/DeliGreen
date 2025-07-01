57e8b4b852c0ea99b49eb83b9510010eb7a17748
Duarss <s160422035@student.ubaya.ac.id>
2025-07-01 08:57:13 +0700

    add order_id and transaction_date on transactions table

--- database/migrations/2025_03_06_151152_create_transactions_table.php
+++ database/migrations/2025_03_06_151152_create_transactions_table.php
@@ -13,8 +13,11 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->uuid('invoice_id')->primary();
+            $table->unsignedBigInteger('order_id')->nullable();
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->dateTime('transaction_date')->nullable();
             $table->enum('payment_method', ['QRIS', 'Credit Card', 'Debit Card', 'e-Wallet'])->nullable();
             $table->decimal('amount', 10, 2);
             $table->enum('payment_status', ['Pending', 'Paid', 'Failed'])->default('Pending');
